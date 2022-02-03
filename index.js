51740a18b0574ba98390568092f72ae23eca72bd
Dmitrii <shokilla@mail.ru>
2022-02-03 10:59:22 +0300

    add gitignore

---
+++ index.js
@@ -0,0 +1,7 @@
+// const colors = require("colors");
+import colors from "colors";
+
+const args = process.argv.slice(2);
+const [a, userName] = args;
+
+console.log(`Hello ${colors.america(userName)}`);
